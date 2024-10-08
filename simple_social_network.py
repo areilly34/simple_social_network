5282b5a035e37cdd5572ae42c2cc0e73d7e2d3e7
Aidan Reilly <areilly16lax@gmail.com>
2024-10-08 14:27:07 -0400

    added simple_social_network.py

---
+++ simple_social_network.py
@@ -0,0 +1,10 @@
+"""A simple social network. Tracks connections between people"""
+
+class Person:
+    """Person in social network
+    Attributes:
+        name (str): persons name
+        connections (set of Persons): other people
+        
+    """
+    
